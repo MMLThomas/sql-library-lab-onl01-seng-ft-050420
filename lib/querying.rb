00883891c9ff69830ae234a7fb27ccd217e14def
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 03:12:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,8 +27,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
    FROM series
-   INNER JOIN characters
-   GROUP BY(series_id)
+   INNER JOIN books
+   ON
+   JOIN
    "
 end
 
