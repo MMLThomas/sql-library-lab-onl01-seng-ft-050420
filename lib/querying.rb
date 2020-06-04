6d587ec0df7a946159dbe099d68bd75846c4cb71
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 03:33:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,7 +40,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title)
    FROM characters
-   INNER JOIN character_books
+   LEFT OUTTER JOIN character_books
    ON characters.id = character_books.character_id
    JOIN books
    ON character_books.book_id = books.id;"
