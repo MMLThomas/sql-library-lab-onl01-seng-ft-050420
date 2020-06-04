78ed4df9341a6f22336efc35d620d5efcbb91997
michael thomas <michael.thomas2014@gmail.com>
2020-06-04 03:22:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,6 +31,9 @@ def select_series_title_with_most_human_characters
    ON series.id = books.series_id
    JOIN characters_books
    ON books.id = characters_books.book_id
+   JOIN characters
+   ON characters_books.character_id = characters.id
+   WHERE species = 'human'
    "
 end
 
